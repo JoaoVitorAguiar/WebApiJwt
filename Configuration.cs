3016c3f621a6270d254b30b45c1c182738ab3dc0
jvAguiarSilva <107854662+jvAguiarSilva@users.noreply.github.com>
2023-08-17 19:58:37 -0300

    feat: create class Configure.cs

---
+++ Configuration.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApiJwt;
+
+public class Configuration
+{
+    // Token - JWT - Json Web Token
+    public static string JwtKey = "ZmVkYWY3ZDg4NjNiNDhlMTk3YjkyODdkNDkyYjcwOGU=";
+    // Sempre que passar este parâmetro na requisição ele será autenticado
+    public static string ApiKeyName = "api_key";
+    public static string ApiKey = "haligdauigfuweluieaeggdlslgeiulggf";
+    public static SmtpConfiguration Smtp = new();
+    public class SmtpConfiguration
+    {
+        public string Host { get; set; }
+        public int Port { get; set; } = 25;
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
