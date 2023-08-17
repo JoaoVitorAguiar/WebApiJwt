f3a7dd6f4bbb90f0cb01ac1d4445c98a22090681
jvAguiarSilva <107854662+jvAguiarSilva@users.noreply.github.com>
2023-08-17 19:22:50 -0300

    feat: add models Role and User

---
+++ Models/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApiJwt.Models;
+
+public class Role
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public IList<User> Users { get; set; }
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Text.Json.Serialization;
+
+namespace WebApiJwt.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public IList<Role> Roles { get; set; }
+}
