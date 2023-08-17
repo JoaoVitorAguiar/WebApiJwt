905b96488cc034a7b1f353b9f1b13e993b67c4d8
jvAguiarSilva <107854662+jvAguiarSilva@users.noreply.github.com>
2023-08-17 19:40:54 -0300

    feat: configure the data context

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using WebApiJwt.Models;
+
+namespace WebApiJwt.Data;
+
+public class DataContext : DbContext
+{
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DataContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+}
