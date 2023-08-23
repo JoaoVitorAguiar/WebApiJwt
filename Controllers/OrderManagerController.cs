6cfe6a0e51582fb60112e4260c53b4ca8b9980c1
jvAguiarSilva <107854662+jvAguiarSilva@users.noreply.github.com>
2023-08-23 14:36:17 -0300

    feat: add migration AddOrderDateRequest and change of OrderController to OrderManagerController

---
+++ Controllers/OrderManagerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiJwt.Data;
+
+namespace WebApiJwt.Controllers;
+
+[ApiController]
+[Authorize(Roles = "manager,admin")]
+public class OrderManagerController : ControllerBase
+{
+    
+    [HttpGet("v1/orders")]
+    public async Task<IActionResult> GetAsync(
+        [FromServices] DataContext context
+        )
+    {
+
+        return Ok();
+
+    }
+}
--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -9,6 +9,7 @@ public class DataContext : DbContext
     public DbSet<Role> Roles { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
     public DataContext(DbContextOptions options) : base(options)
     {
         
