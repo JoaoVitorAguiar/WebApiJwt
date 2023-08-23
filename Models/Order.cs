06b0f4e25054a3f0c55a686f288bb237a4c942bf
jvAguiarSilva <107854662+jvAguiarSilva@users.noreply.github.com>
2023-08-23 14:14:17 -0300

    fix: change the Order, add prop DateRequest

--- Models/Order.cs
+++ Models/Order.cs
@@ -6,10 +6,18 @@ namespace WebApiJwt.Models;
 [Table("Orders")]
 public class Order
 {
+    public Order(Product product, User user, DateTime dateRequest)
+    {
+        Product = product;
+        User = user;
+        DateRequest = dateRequest;
+    }
+
     [Key]
     public int Id { get; set; }
     [Key]
     public Product Product { get; set; }
     [Key]
     public User User { get; set; }
+    public DateTime DateRequest  { get; set; }
 }
